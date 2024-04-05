6f2bd9c8595c95af753041e12e6e3b93509f29b3
bogdan-karpovic <158283447+bogdan-karpovic@users.noreply.github.com>
2024-04-05 03:02:20 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,19 @@
+// 1
+const userName = 'Bogdan';
+const password = 'secret';
+const resultPrompt = prompt('Введіть пароль:');
+console.log(resultPrompt === password);
+
+
+// 2 
+const x = 5;
+const y = 3;
+
+alert(x - y);
+alert(x + y);
+alert(x * y);
+alert(x / y);
+
+
+
+
